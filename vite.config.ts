d2e285265ac56455ccb2bca13cd7f21e1c51e8fa
Tejas Tarte <tejasvitthaltarte@gmail.com>
2025-07-28 23:58:11 +0530

    2nd commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ✅ For tejastarte001.github.io root domain
   css: {
-    postcss: './postcss.config.cjs'
-  }
+    postcss: './postcss.config.cjs',
+  },
 })
